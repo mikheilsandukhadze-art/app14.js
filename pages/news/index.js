37ef36cef86efe56e205b236ef4a4fc129ecef99
mikheilsandukhadze-art <mikheilsandukhadze@gmail.com>
2026-08-28 21:13:06 +0400

    code commit

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,20 @@
 //our-domain.com/news
+import Link from "next/link"; 
+import { Fragment } from "react";
 
 function NewsPage() {
     return (
-        <h1>The News Page</h1>
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">
+                        NextJs Is A Great Framework
+                    </Link>
+                </li>
+                <li>Something else</li>
+            </ul>
+        </Fragment>
     )
 }
 
